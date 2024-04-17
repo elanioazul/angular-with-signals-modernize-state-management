6d6264ade969b15d1b49eb885f4ada0bce4fe9aa
Hugo Herrador <hugo.herrador@inetum.com>
2024-04-17 11:57:35 +0200

    -create-an-angular-signal-which-stores-a-value

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, RouterOutlet],
+  template: `
+  <h1>Angular signals</h1>
+  
+  `,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-with-signals-modernize-state-management';
+  item = signal<string | undefined>(undefined)
 }
