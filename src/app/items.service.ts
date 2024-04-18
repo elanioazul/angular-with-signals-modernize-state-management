a62c10f5d9a8501d86cdd256084882510e183507
Hugo Herrador <hugo.herrador@inetum.com>
2024-04-18 17:17:31 +0200

    angular-synchronize-angular-signal-or-computed-value-with-localstorage-using-effect

--- src/app/items.service.ts
+++ src/app/items.service.ts
@@ -1,17 +1,26 @@
 import { Injectable, effect, signal, untracked } from '@angular/core';
 
+type Item = {
+  id: number,
+  name: string
+}
 @Injectable({
   providedIn: 'root'
 })
 export class ItemsService {
-  #items = signal([
-    {id: 1, name: 'Andy'},
-    {id: 2, name: 'Hugo'},
-    {id: 3, name: 'Willy'},
+  // #items = signal([
+  //   {id: 1, name: 'Andy'},
+  //   {id: 2, name: 'Hugo'},
+  //   {id: 3, name: 'Willy'},
 
-  ])
+  // ])
+  #items = signal(JSON.parse(localStorage.getItem('items')!))
 
   items = this.#items.asReadonly();
+
+  syncronyzeItemsAtStorage = effect(() => {
+    localStorage.setItem('items', JSON.stringify(this.#items()))
+  })
 
   clearItems() {
     this.#items.set([])
